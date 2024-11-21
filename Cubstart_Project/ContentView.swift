7d2aaf83ec06aa09cbcc96483f761cecf5acb223
Iwan000 <12112504@mail.sustech.edu.cn>
2024-11-21 12:53:32 -0800

    HomeView 11.21

--- Cubstart_Project/ContentView.swift
+++ Cubstart_Project/ContentView.swift
@@ -10,15 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            SmartHomeView()
+                .navigationTitle("Smart Home")
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
