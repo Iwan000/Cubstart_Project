ad5aa4e48e10eeec16fdea09c084bfe2cfce9a15
YHC66 <yhc0720@berkeley.edu>
2024-11-22 22:41:52 -0800

    ac

---
+++ icontroller/icontroller/icontrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cubstart_ProjectApp.swift
+//  Cubstart_Project
+//
+//  Created by 郭城 on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Cubstart_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ icontroller/icontrollerTests/icontrollerTests.swift
@@ -0,0 +1,16 @@
+//
+//  icontrollerTests.swift
+//  icontrollerTests
+//
+//  Created by cyh on 11/20/24.
+//
+
+import Testing
+
+struct icontrollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
